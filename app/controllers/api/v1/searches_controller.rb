0ae93e4e9be722ad4d6aba2a2a7b3f576c26ab00
Francesco Romano <43118093+fwromano@users.noreply.github.com>
2023-10-13 13:25:53 -0500

    advanced search

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -6,10 +6,25 @@ module Api
         skip_before_action :verify_authenticity_token
   
         def index
-          query = params[:query]
+            query = params[:query]
+            field = params[:field] 
+            
+            case field
+            when "application_name"
+              @results = Applicant.where("lower(application_name) LIKE ?", "%#{query.downcase}%")
+            when "application_cas_id"
+              @results = Applicant.where("application_cas_id LIKE ?", "%#{query}%")
+            when "application_citizenship_country"
+                @results = Applicant.where("lower(application_citizenship_country) LIKE ?", "%#{query.downcase}%")
+            when "application_degree"
+                @results = Applicant.where("lower(application_degree) LIKE ?", "%#{query.downcase}%")
+            else
+              @results = Applicant.where("lower(application_name) LIKE ?", "%#{query.downcase}%")
+            end
+            
 
-          @results = Applicant.where("application_name LIKE ?", "%#{query}%")
-          render json: @results
+            puts @results
+            render json: @results
         end
       end
     end
