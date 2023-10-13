7b90c5c7e4c657deda8647d96526662745d605e5
bmnz77 <110623936+bmnz77@users.noreply.github.com>
2023-10-13 15:35:51 -0500

    added basic file uplaod functionality, saves file locally

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'uploads/new'
+  #get 'uploads/create'
+  resources :uploads, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,6 +18,13 @@ Rails.application.routes.draw do
     end
   end
   # Defines the root path route ("/")
+
+  #routes for file upload
+  # post 'uploads', to: 'uploads#new'
+
+
+
+
   # root "posts#index"
 
 end
