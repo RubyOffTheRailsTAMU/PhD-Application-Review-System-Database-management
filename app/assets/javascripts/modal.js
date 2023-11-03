f11a129656bc097a9c4d5f2fbf4249eeacb15dc0
bmnz77 <110623936+bmnz77@users.noreply.github.com>
2023-11-03 13:49:24 -0500

    popup

--- app/assets/javascripts/modal.js
+++
@@ -1,7 +0,0 @@
-// app/assets/javascripts/modal.js
-$(document).ready(function() {
-    $("#show-confirmation-modal").on("click", function() {
-      $("#confirmation-modal").modal("show");
-    });
-  });
-  
