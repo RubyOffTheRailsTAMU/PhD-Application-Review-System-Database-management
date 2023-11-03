bfc87f2c26f0f0ec951779b70df4553005db1d53
bmnz77 <110623936+bmnz77@users.noreply.github.com>
2023-11-03 11:43:53 -0500

    comment jwt to push

--- config/initializers/jwt.rb
+++ config/initializers/jwt.rb
@@ -2,5 +2,5 @@ require 'jwt'
 require 'base64'
 
 JWT_ALGORITHM = 'RS256'
-JWT_PUBLIC_KEY = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('public_key.pem')))
-#JWT_PUBLIC_KEY = OpenSSL::PKey::RSA.new(Base64.strict_decode64(ENV['PUBLIC_KEY']))
+#JWT_PUBLIC_KEY = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('public_key.pem')))
+JWT_PUBLIC_KEY = OpenSSL::PKey::RSA.new(Base64.strict_decode64(ENV['PUBLIC_KEY']))
