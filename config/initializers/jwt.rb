3ad72f877305468afcfd1870cd5a223926983d3b
XRFXRF <Felixxrf@outlook.com>
2023-11-03 12:25:23 -0500

    add comment in jwt.rb

--- config/initializers/jwt.rb
+++ config/initializers/jwt.rb
@@ -2,5 +2,8 @@ require 'jwt'
 require 'base64'
 
 JWT_ALGORITHM = 'RS256'
+# Get the key locally
 # JWT_PUBLIC_KEY = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('public_key.pem')))
+
+# Get the key on Heroku
 JWT_PUBLIC_KEY = OpenSSL::PKey::RSA.new(Base64.strict_decode64(ENV['PUBLIC_KEY']))
